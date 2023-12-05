f0b1eee8ede91e5e0c806407ea141666651939ef
Alejandro-Elias <alejandroelias25@gmail.com>
2023-12-05 12:52:24 -0300

    fix: arregla vista de productos

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -5,7 +5,6 @@ module.exports = {
     dashboard: (req, res) => {
 
         const productos = leerJSON('productos');
-        console.log(productos);
 
         return res.render('./dashboard', {productos})
     }

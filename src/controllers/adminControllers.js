3144508b7aa8a4fdc2e635079580d59ec1ca8234
Alejandro-Elias <alejandroelias25@gmail.com>
2023-12-31 20:42:11 -0300

    feat: agrega funcionalidad search dashboard

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -7,5 +7,13 @@ module.exports = {
         const productos = leerJSON('productos');
 
         return res.render('./dashboard', {productos})
-    }
+    },
+    search: (req, res) => {        
+        const productos = leerJSON('productos');
+		const {keywords} = req.query;
+		return res.render('dashboardFilter', {
+			productos : productos.filter(producto => producto.nombre.toLowerCase().includes(keywords.toLowerCase())), 
+			keywords            
+		})
+	}
 }

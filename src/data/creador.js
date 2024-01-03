10e984795beb6c444e488b044fa20d71e1af4f6c
SashaToledo <113081224+SashaToledo@users.noreply.github.com>
2024-01-03 15:59:34 -0300

    arreglando errores

--- src/data/creador.js
+++ src/data/creador.js
@@ -1,14 +1,14 @@
 const crypto = require ('crypto');
 
 function creador( nombre, imagen, categoria, precio, stock, sabores, descuento, descripcion){
-    this.id = crypto.randomUUID(),
-    this.nombre = nombre.trim(),
-    this.imagen = imagen ? imagen.filename : null,
-    this.categoria = categoria.trim() ,
-    this.precio = +precio.trim(),
-    this.stock = +stock.trim() ,
-    this.sabores = sabores.trim() ,
-    this.descuento =  +descuento.trim() ,
-    this.descripcion = descripcion.trim() 
+    this.id = crypto.randomUUID();
+    this.nombre = nombre ? nombre.trim() : '';
+    this.imagen = imagen ? imagen.filename : null;
+    this.categoria = categoria ? categoria.trim() : '';
+    this.precio = precio ? +precio.trim() : 0;
+    this.stock = stock ? +stock.trim() : 0;
+    this.sabores = sabores ? sabores.trim() : '';
+    this.descuento = descuento ? +descuento.trim() : 0;
+    this.descripcion = descripcion ? descripcion.trim() : '';
 }
 module.exports = creador;

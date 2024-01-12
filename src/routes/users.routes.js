4619e445f3f8ee6532146259e015935032cd143a
Nicoofla <nikoofla@gmail.com>
2024-01-12 15:51:04 -0300

    add file middlewares

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {login, register, dashboard, processRegister, processLogin, logout, profile} = require('../controllers/usersController');
+const {login, register, processRegister, processLogin, logout, profile} = require('../controllers/usersController');
 const router = express.Router();
 
 /* GET users listing. */

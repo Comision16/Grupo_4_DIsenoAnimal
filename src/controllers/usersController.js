302087656c4364c787d711282aa59af3ca037fb9
Alejandro-Elias <alejandroelias25@gmail.com>
2024-02-19 18:35:37 -0300

    refactoriza login y register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,174 +3,181 @@ const User = require("../data/User");
 const Reserva = require("../data/reserva")
 const { existsSync, unlinkSync } = require('fs');
 const { leerJSON, escribirJSON } = require("../data");
-
 const db = require("../database/models")
+const bcryptjs = require('bcryptjs')
+
 
 module.exports = {
-    login : (req, res) => {
+    login: (req, res) => {
         return res.render('users/login')
-        
+
     },
-    processLogin : (req, res) => {
-        
+    processLogin: (req, res) => {
+
         const errors = validationResult(req);
+        const { email, remember } = req.body
 
-        if(errors.isEmpty()){
+        if (errors.isEmpty()) {
 
-        const dato = req.body
 
-        console.log(dato);
-
-            const {id, name, email, mascota, especie, imagen, role} = leerJSON('users').find(user => user.email == dato.email)
-
-            req.session.userLogin = {
-                id,
-                name,
-                email,
-                mascota,
-                especie,
-                imagen,
-                role
-            }
-
-            dato.remember && res.cookie('animalDeUs3r_Cancat', req.session.userLogin, {
-                maxAge : 1000 * 60 * 60
+            db.User.findOne({
+                where: {
+                    email
+                }
             })
 
-            return res.redirect ('/usuarios/perfil')
+                .then(({ id, name, email, roleId, image }) => {
+                    req.session.userLogin = {
+                        id,
+                        name,
+                        email,
+                        image,
+                        role: +roleId
+                    }
+                    remember && res.cookie('animalDeUs3r_Cancat', req.session.userLogin, {
+                        maxAge: 1000 * 60 * 60
+                    })
+                    return res.redirect('/usuarios/perfil')
+                })
+                .catch(error => console.log(error))
 
-        }else{
-            return res.render ('users/login', {
-                errors : errors.mapped()
+        } else {
+            return res.render('users/login', {
+                errors: errors.mapped()
             })
         }
     },
 
-    register : (req, res) => {
+    register: (req, res) => {
         return res.render('users/register', {
         })
     },
 
-    processRegister : (req,res) => {
+    processRegister: (req, res) => {
         const errors = validationResult(req);
-        const {name, email, password} = req.body;
+        const { name, email, password } = req.body;
 
-        if(errors.isEmpty()){
+        if (errors.isEmpty()) {
 
-            const users = leerJSON('users');
-            const newUser = new User(name, email, password);
-            
-            users.push(newUser);
+            db.User.create({
+                name,
+                email,
+                password : bcryptjs.hashSync(password.trim(), 10),
+                roleId : 1,
+                mascota : "",
+                especie : "",
+                imagen : ""
+            })
+            .then(user => {
+                console.log(user);
+            return res.redirect('/usuarios/ingreso')                
+            })
 
-            escribirJSON(users, 'users')
 
-            return res.redirect('/usuarios/ingreso')
-            
 
-        }else{
-            return res.render('users/register',{
-                old : req.body,
-                errors : errors.mapped()
+        } else {
+            return res.render('users/register', {
+                old: req.body,
+                errors: errors.mapped()
             })
         }
 
     },
-    logout :  (req, res) => {
+    logout: (req, res) => {
         req.session.destroy();
-        res.cookie('animalDeUs3r_Cancat',null,{
-            maxAge : -1
+        res.cookie('animalDeUs3r_Cancat', null, {
+            maxAge: -1
         })
 
         return res.redirect("/")
     },
-    profile : (req,res) => {    
+    profile: (req, res) => {
 
         const imagenUser = req.session.userLogin
-        
 
-        const {email} = req.session.userLogin
+        const { email } = req.session.userLogin
 
         const users = leerJSON('users');
-        
-        const usuario = users.find( user => user.email == email)
+
+        const usuario = users.find(user => user.email == email)
 
         const especies = leerJSON("especie")
 
         return res.render("users/perfil", {
             ...usuario,
-        imagenUser,
-        especies
+            ...imagenUser,
+            especies
         })
     },
-    update : (req,res) => {       
-        const {name, email, mascota, especie} = req.body;
+    update: (req, res) => {
+        const { name, email, mascota, especie } = req.body;
         const { id } = req.params;
 
         const errors = validationResult(req);
 
-        if(errors.isEmpty()) {
-        
-        const imagenDelete = req.file ? req.file.fieldname : null  ;
+        if (errors.isEmpty()) {
 
-        const usuarios = leerJSON('users');
+            const imagenDelete = req.file ? req.file.fieldname : null;
 
-        const userUpdate = usuarios.map(usuario => {
-            if (usuario.id == id) {
+            const usuarios = leerJSON('users');
 
-            (imagenDelete && existsSync('public/images/' + usuario.imagen)) && unlinkSync('public/images/' + usuario.imagen)
+            const userUpdate = usuarios.map(usuario => {
+                if (usuario.id == id) {
 
-            usuario.name = name.trim(),
-            usuario.email = email.trim(),
-            usuario.mascota = mascota.trim(),
-            usuario.especie = especie,    
-            usuario.imagen =  req.file ? req.file.filename : usuario.imagen            
-            }           
+                    (imagenDelete && existsSync('public/images/' + usuario.imagen)) && unlinkSync('public/images/' + usuario.imagen)
 
-            return usuario
-        });        
+                    usuario.name = name.trim(),
+                        usuario.email = email.trim(),
+                        usuario.mascota = mascota.trim(),
+                        usuario.especie = especie,
+                        usuario.imagen = req.file ? req.file.filename : usuario.imagen
+                }
 
-        escribirJSON(userUpdate, 'users')  
+                return usuario
+            });
 
-        const users = leerJSON('users');
+            escribirJSON(userUpdate, 'users')
 
-        const usuario = users.find( user => user.id == id)
+            const users = leerJSON('users');
 
-        const especies = leerJSON("especie")
+            const usuario = users.find(user => user.id == id)
 
-        return res.render("users/perfil", {
-            old : req.body,
-            ...usuario,
-            especies
-        })
+            const especies = leerJSON("especie")
+
+            return res.render("users/perfil", {
+                old: req.body,
+                ...usuario,
+                especies
+            })
 
         } else {
             const datosUsuario = req.session.userLogin
 
             console.log(datosUsuario);
 
-            return res.render('users/perfil',{
-                old : req.body,
-                errors : errors.mapped(),
+            return res.render('users/perfil', {
+                old: req.body,
+                errors: errors.mapped(),
                 ...datosUsuario
             })
         }
     },
-    dashboardUsuarios : (req, res) => {
+    dashboardUsuarios: (req, res) => {
         const users = leerJSON('users');
 
         return res.render('dashboardUser', {
             users
         });
     },
-    gerarquia : (req, res) => {
+    gerarquia: (req, res) => {
         const { id } = req.params;
-        
+
         const users = leerJSON('users');
 
         const userUpdate = users.map(usuario => {
             if (usuario.id == id) {
                 if (req.body.admin) {
-                    usuario.role = "admin"                    
+                    usuario.role = "admin"
                 } else {
                     usuario.role = "user"
                 }
@@ -178,48 +185,48 @@ module.exports = {
             return usuario
         });
 
-        escribirJSON(userUpdate, 'users')  
+        escribirJSON(userUpdate, 'users')
 
         return res.render('dashboardUser', {
             users
         });
     },
-    reserva : (req, res) => {
+    reserva: (req, res) => {
 
-        const imagenUser = req.session.userLogin        
+        const imagenUser = req.session.userLogin
 
-        const {email} = req.session.userLogin
+        const { email } = req.session.userLogin
 
         const users = leerJSON('users');
-        
-        const usuario = users.find( user => user.email == email)
+
+        const usuario = users.find(user => user.email == email)
 
         const especies = leerJSON("especie")
 
         res.render("users/reserva", {
-            old : req.body,
+            old: req.body,
             ...usuario,
             imagenUser,
             especies
         })
     },
-    reservar :(req, res) => {
-        const {name, email, mascota, especie, fecha, hora } = req.body;
+    reservar: (req, res) => {
+        const { name, email, mascota, especie, fecha, hora } = req.body;
 
         const reserva = leerJSON('reserva');
-        const newBooking = new Reserva(name, email,  mascota, especie, fecha, hora);
-        
+        const newBooking = new Reserva(name, email, mascota, especie, fecha, hora);
+
         reserva.push(newBooking);
 
         escribirJSON(reserva, 'reserva')
 
-        const {id} = req.session.userLogin ? req.session.userLogin : 1
+        const { id } = req.session.userLogin ? req.session.userLogin : 1
 
         const users = leerJSON('users');
 
-        const usuario = users.find( user => user.id == id)
-        
+        const usuario = users.find(user => user.id == id)
 
-        return res.render("users/reservado", {...usuario} )
+
+        return res.render("users/reservado", { ...usuario })
     }
 }

606fe8aac1a1b53cae7b78eb28aa5b28f0465a57
Alejandro-Elias <alejandroelias25@gmail.com>
2024-02-21 17:44:34 -0300

    refactoriza creacion de productos

--- src/controllers/productsController/store.js
+++ src/controllers/productsController/store.js
@@ -1,7 +1,46 @@
 
 const fs = require('fs');
+const db = require("../../database/models")
 
-function pushProducts (parametro){
+module.exports = (req, res) => {
+
+    const { nombre, categoria, precio, stock, sabores, descuento, descripcion } = req.body;
+
+    const image1 = req.files ? req.files.image1 : null
+    const image2 = req.files ? req.files.image2 : null
+
+    db.Product.create({
+        name : nombre.trim(),
+        price : precio,
+        discount : +descuento,
+        description : descripcion.trim(),
+        specieId : +categoria        
+    })
+    .then(producto => {
+        db.stock.create({
+            amount : +stock,
+            flavorId : +sabores,
+            productId : producto.id
+        })
+
+        db.Image_products.create({
+            file : image1[0].filename,
+            productId : producto.id
+        })
+
+        db.Image_products.create({
+            file : image2[0].filename,
+            productId : producto.id
+        })
+
+        return res.redirect('/admin/dashboard')
+    })
+}
+
+
+
+
+/* function pushProducts (parametro){
 fs.writeFileSync("./src/data/productos.json", JSON.stringify(parametro, null, 3), "utf-8");}
 
 module.exports = (req, res) =>  {
@@ -15,4 +54,4 @@ module.exports = (req, res) =>  {
         productos.push(nuevoCreador);
         pushProducts(productos);
         return res.redirect('/admin/dashboard')
-    } 
+    }  */

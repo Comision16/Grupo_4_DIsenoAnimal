735eb5b7c5202e8371bdb3b580b8e41f9210a2fd
Alejandro-Elias <alejandroelias25@gmail.com>
2024-02-26 11:10:38 -0300

    agrega enviar imagen de usuario a la vista filtrados

--- src/controllers/productsController/filtrados.js
+++ src/controllers/productsController/filtrados.js
@@ -1,35 +1,35 @@
-const {leerJSON} = require("../../data")
+const { leerJSON } = require("../../data")
 
 const db = require('../../database/models');
 
 
 
-module.exports = (req, res) =>  {
+module.exports = (req, res) => {
 
     /* return res.send(req.params) */
-        
-    
-    const {id} = req.session.userLogin ? req.session.userLogin : 0
 
-        const users = leerJSON('users');
 
-        const usuario = users.find( user => user.id == id)        
-        
+    const { id } = req.session.userLogin 
 
-        const {categoria} = req.params
+    const usuario = db.User.findByPk(id)
 
-        db.Product.findAll({
-            include: [
-                "Image_products",
-                "product_species",
-            ]
-        })
-        .then(productos => {
-            
+    const { categoria } = req.params
+
+    const productos = db.Product.findAll({
+        include: [
+            "Image_products",
+            "product_species",
+        ]
+    })
+
+    Promise.all(([productos, usuario]))
+
+        .then(([productos, usuario]) => {
+
             const productosFiltrados = productos.filter(producto => producto.product_species.name == categoria);
-    
+
             // return res.send(nameSpecie)
-    
+
             // return res.send(productosFiltrados)
             return res.render('products/productFilter', {
                 productos: productosFiltrados,
@@ -37,4 +37,4 @@ module.exports = (req, res) =>  {
             });
         })
         .catch(error => console.log(error));
-    }
+}

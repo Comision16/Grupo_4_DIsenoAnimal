f35435976376d84e634c57130d3ea84dfb7e3830
Alejandro-Elias <alejandroelias25@gmail.com>
2024-03-23 22:19:16 -0300

    arregla crear producto

--- src/controllers/productsController/store.js
+++ src/controllers/productsController/store.js
@@ -1,24 +1,18 @@
 const db = require("../../database/models");
 const { validationResult } = require('express-validator');
 
-
 module.exports = (req, res) => {
-
     const errors = validationResult(req).mapped();
-    const error = validationResult(req);
 
     console.log(req.body);
 
     if (Object.keys(errors).length > 0) {
-        const especies = db.Specie.findAll()
+        const especiesPromise = db.Specie.findAll();
+        const mascotasPromise = db.Pet.findOne();
+        const saboresPromise = db.Flavor.findAll();
+        const filingPromise = db.Filing.findAll();
 
-        const mascotas = db.Pet.findOne()
-
-        const sabores = db.Flavor.findAll()
-
-        const filing = db.Filing.findAll()
-
-        Promise.all([especies, mascotas, sabores, filing])
+        Promise.all([especiesPromise, mascotasPromise, saboresPromise, filingPromise])
             .then(([especies, mascotas, sabores, filing]) => {
                 return res.render('products/product-create', {
                     especies,
@@ -27,68 +21,73 @@ module.exports = (req, res) => {
                     filing,
                     errors,
                     old: req.body
-                })
+                });
             })
+            .catch(err => {
+                console.error('Error fetching data from database:', err);
+                return res.status(500).send('Internal Server Error');
+            });
+    } else {
+        const { nombre, categoria, precio, stock, sabores, descuento, descripcion, brand, measure, value } = req.body;
+        const image1 = req.files.image1 == undefined ? null : req.files.image1[0].filename;
+        const image2 = req.files.image2 == undefined ? null : req.files.image2[0].filename;
 
-
-    }
-    const { nombre, categoria, precio, stock, sabores, descuento, descripcion, brand, measure, value } = req.body;
-
-    const image1 = req.files.image1 == undefined ? "null" : req.files.image1;
-    const image2 = req.files.image2 == undefined ? "null" : req.files.image2;
-
-    if (!error) {
-        db.Brand.create({
-        name: brand
-    }).then(brand => {
-        db.Product.create({
-            name: nombre.trim(),
-            price: +precio,
-            discount: +descuento,
-            description: descripcion.trim(),
-            specieId: +categoria,
-            value: value,
-            brandId: brand.id,
-            filingId: measure,
-        })
+        db.Brand.create({ name: brand })
+            .then(brand => {
+                return db.Product.create({
+                    name: nombre.trim(),
+                    price: +precio,
+                    discount: +descuento,
+                    description: descripcion.trim(),
+                    specieId: +categoria,
+                    value,
+                    brandId: brand.id,
+                    filingId: measure,
+                });
+            })
             .then(producto => {
-                db.stock.create({
+                return db.stock.create({
                     amount: +stock,
                     flavorId: +sabores,
                     productId: producto.id
                 });
-
+            })
+            .then(() => {
                 if (image1) {
-                    db.Image_products.create({
-                        file: image1[0].filename,
-                        productId: producto.id,
+                    return db.Image_products.create({
+                        file: image1,
+                        productId: req.params.id,
                         primary: 1
                     });
                 } else {
-                    db.Image_products.create({
+                    return db.Image_products.create({
                         file: null,
-                        productId: producto.id,
+                        productId: req.params.id,
                         primary: 1
                     });
                 }
-
+            })
+            .then(() => {
                 if (image2) {
-                    db.Image_products.create({
-                        file: image2[0].filename,
+                    return db.Image_products.create({
+                        file: image2,
                         productId: producto.id,
                         primary: 2
                     });
                 } else {
-                    db.Image_products.create({
+                    return db.Image_products.create({
                         file: null,
-                        productId: producto.id,
+                        productId: req.params.id,
                         primary: 2
                     });
                 }
+            })
+            .then(() => {
                 return res.redirect('/admin/dashboard');
+            })
+            .catch(err => {
+                console.error('Error creating product:', err);
+                return res.status(500).send('Internal Server Error');
             });
-    });
     }
-
-    
-}
+};
